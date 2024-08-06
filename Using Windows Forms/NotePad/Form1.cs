4dac78448e779d67b1d8ce70cd65cf75d15aad19
Abdelrahman Kamar <74204501+AbdelrahmanAhmedKamar@users.noreply.github.com>
2024-08-06 10:28:17 +0300

    Add files via upload

---
+++ Using Windows Forms/NotePad/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.Filter = "Text Files | *.TXT";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+                MessageBox.Show("Saved Successfully", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(richTextBox1.Text, new Font("Tahoma", 18), Brushes.Black, 10, 20);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pageSetupDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            printDialog1.ShowDialog();
+        }
+
+
+    }
+}
