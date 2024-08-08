6da502dc7c192c111485f0e240d18805a1c23e5d
Abdelrahman Kamar <74204501+AbdelrahmanAhmedKamar@users.noreply.github.com>
2024-08-08 16:22:51 +0300

    Add files via upload

---
+++ Using Windows Forms/Stored Procedures/Stored Procedures/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace Stored_Procedures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtNumberOfPages = new System.Windows.Forms.TextBox();
+            this.dtPublishDate = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(129, 404);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(650, 157);
+            this.dataGridView1.TabIndex = 34;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Purple;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnClose.Location = new System.Drawing.Point(625, 324);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(92, 39);
+            this.btnClose.TabIndex = 33;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Purple;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnUpdate.Location = new System.Drawing.Point(511, 324);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(92, 39);
+            this.btnUpdate.TabIndex = 32;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Purple;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDelete.Location = new System.Drawing.Point(397, 324);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(92, 39);
+            this.btnDelete.TabIndex = 31;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Purple;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAdd.Location = new System.Drawing.Point(283, 324);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(92, 39);
+            this.btnAdd.TabIndex = 30;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.BackColor = System.Drawing.Color.Purple;
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnNew.Location = new System.Drawing.Point(169, 324);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(92, 39);
+            this.btnNew.TabIndex = 29;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = false;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(178, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 20);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Date of Publishing:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(178, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 20);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Number of Pages:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(178, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Book Author:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(178, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Book Title:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(178, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Book ID:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(230, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "CRUD Operation in C# Using SP Con";
+            // 
+            // txtID
+            // 
+            this.txtID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(335, 122);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(330, 26);
+            this.txtID.TabIndex = 20;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(335, 156);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(330, 26);
+            this.txtTitle.TabIndex = 22;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAuthor.Location = new System.Drawing.Point(335, 192);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(330, 26);
+            this.txtAuthor.TabIndex = 24;
+            // 
+            // txtNumberOfPages
+            // 
+            this.txtNumberOfPages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNumberOfPages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberOfPages.Location = new System.Drawing.Point(335, 226);
+            this.txtNumberOfPages.Name = "txtNumberOfPages";
+            this.txtNumberOfPages.Size = new System.Drawing.Size(330, 26);
+            this.txtNumberOfPages.TabIndex = 26;
+            // 
+            // dtPublishDate
+            // 
+            this.dtPublishDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtPublishDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtPublishDate.Location = new System.Drawing.Point(335, 262);
+            this.dtPublishDate.Name = "dtPublishDate";
+            this.dtPublishDate.Size = new System.Drawing.Size(330, 26);
+            this.dtPublishDate.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(842, 573);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.dtPublishDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNumberOfPages);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Stored Procedures";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtNumberOfPages;
+        private System.Windows.Forms.DateTimePicker dtPublishDate;
+    }
+}
+
---
+++ Using Windows Forms/Stored Procedures/Stored Procedures/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Stored_Procedures
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(@"Server=.\SQLEXPESS;Database=LibraryDB;Integrated Security=true;");
+        SqlCommand Cmd;
+        SqlDataAdapter Da;
+        DataTable Dt = new DataTable();
+
+        void LoadAllRecord() 
+        {
+            Dt.Clear();
+            Cmd = new SqlCommand("SelectBooks", cn);
+            Cmd.CommandType = CommandType.StoredProcedure;
+            Da = new SqlDataAdapter(Cmd);
+            Da.Fill(Dt);
+
+            this.dataGridView1.DataSource = Dt;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAllRecord();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Cmd = new SqlCommand("InsertBook", cn);
+            Cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter[] Param = new SqlParameter[5];
+
+            Param[0] = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            Param[0].Value = txtID.Text;
+
+            Param[1] = new SqlParameter("@Title", SqlDbType.VarChar, 75);
+            Param[1].Value = txtTitle.Text;
+
+            Param[2] = new SqlParameter("@Author", SqlDbType.VarChar, 100);
+            Param[2].Value = txtAuthor.Text;
+
+            Param[3] = new SqlParameter("@Pages_Number", SqlDbType.Int);
+            Param[3].Value = txtNumberOfPages.Text;
+
+            Param[4] = new SqlParameter("@Publish_Date", SqlDbType.DateTime);
+            Param[4].Value = dtPublishDate.Text;
+
+            Cmd.Parameters.AddRange(Param);
+
+            cn.Open();
+            Cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Added Sucessfully !", "Add", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadAllRecord();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Cmd = new SqlCommand("DeleteBook", cn);
+            Cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter Param = new SqlParameter();
+
+            Param = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            Param.Value = txtID.Text;
+
+            Cmd.Parameters.Add(Param);
+
+            cn.Open();
+            Cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Deleted Sucessfully !", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadAllRecord();
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            txtID.Text = dataGridView1.CurrentRow.Cells["ID"].Value.ToString();
+            txtTitle.Text = dataGridView1.CurrentRow.Cells["Title"].Value.ToString();
+            txtAuthor.Text = dataGridView1.CurrentRow.Cells["Author"].Value.ToString();
+            txtNumberOfPages.Text = dataGridView1.CurrentRow.Cells["Pages_Number"].Value.ToString();
+            dtPublishDate.Text = dataGridView1.CurrentRow.Cells["Publish_Date"].Value.ToString();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtID.Clear();
+            txtTitle.Clear();
+            txtAuthor.Clear();
+            txtNumberOfPages.Clear();
+            dtPublishDate.ResetText();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Cmd = new SqlCommand("UpdateBook", cn);
+            Cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter[] Param = new SqlParameter[5];
+
+            Param[0] = new SqlParameter("@ID", SqlDbType.VarChar, 50);
+            Param[0].Value = txtID.Text;
+
+            Param[1] = new SqlParameter("@Title", SqlDbType.VarChar, 75);
+            Param[1].Value = txtTitle.Text;
+
+            Param[2] = new SqlParameter("@Author", SqlDbType.VarChar, 100);
+            Param[2].Value = txtAuthor.Text;
+
+            Param[3] = new SqlParameter("@Pages_Number", SqlDbType.Int);
+            Param[3].Value = txtNumberOfPages.Text;
+
+            Param[4] = new SqlParameter("@Publish_Date", SqlDbType.DateTime);
+            Param[4].Value = dtPublishDate.Text;
+
+            Cmd.Parameters.AddRange(Param);
+
+            cn.Open();
+            Cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Updated Sucessfully !", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadAllRecord();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
