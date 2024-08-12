d1e12d9fed0b01c7b158e375c6c215e18532b7a9
Abdelrahman Kamar <74204501+AbdelrahmanAhmedKamar@users.noreply.github.com>
2024-08-12 10:30:22 +0300

    Add files via upload

---
+++ Using Windows Forms/Reporting/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Reporting
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(@"Server=.\SQLEXPESS;Database=Emloyees;Integrated Security=true;");
+        SqlDataAdapter Da;
+        DataTable Dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Da = new SqlDataAdapter("Select * From tbl_employees", cn);
+            Da.Fill(Dt);
+            dataGridView1.DataSource = Dt;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Dt.Clear();
+            Da = new SqlDataAdapter("Select * From tbl_employees Where convert(varchar,ID)+Fullname+Address+Job+Convert(varchar,age) Like '%"+textBox1.Text+"%'", cn);
+            Da.Fill(Dt);
+            dataGridView1.DataSource = Dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ReportForm frm = new ReportForm();
+                        Da = new SqlDataAdapter("Select * From tbl_employees", cn);
+            Da.Fill(frm.EmloyeesDataSet.tbl_employees);
+            frm.reportViewer1.RefreshReport();
+            frm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReportForm frm = new ReportForm();
+            Da = new SqlDataAdapter("Select * From tbl_employees Where ID ='" + dataGridView1.CurrentRow.Cells[0].Value.ToString() + "'", cn);
+            Da.Fill(frm.EmloyeesDataSet.tbl_employees);
+            frm.reportViewer1.RefreshReport();
+            frm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmSearchResult frm = new frmSearchResult();
+            Da = new SqlDataAdapter("Select * From tbl_employees Where convert(varchar,ID)+Fullname+Address+Job+Convert(varchar,age) Like '%" + textBox1.Text + "%'", cn);
+            Da.Fill(frm.EmloyeesDataSet.tbl_employees);
+            frm.reportViewer1.RefreshReport();
+            frm.Show();
+        }
+    }
+}
---
+++ Using Windows Forms/Reporting/ReportForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Reporting
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.EmloyeesDataSet = new Reporting.EmloyeesDataSet();
+            this.tbl_employeesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_employeesTableAdapter = new Reporting.EmloyeesDataSetTableAdapters.tbl_employeesTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.EmloyeesDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_employeesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.tbl_employeesBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Reporting.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // EmloyeesDataSet
+            // 
+            this.EmloyeesDataSet.DataSetName = "EmloyeesDataSet";
+            this.EmloyeesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbl_employeesBindingSource
+            // 
+            this.tbl_employeesBindingSource.DataMember = "tbl_employees";
+            this.tbl_employeesBindingSource.DataSource = this.EmloyeesDataSet;
+            // 
+            // tbl_employeesTableAdapter
+            // 
+            this.tbl_employeesTableAdapter.ClearBeforeFill = true;
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "ReportForm";
+            this.Text = "ReportForm";
+            this.Load += new System.EventHandler(this.ReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmloyeesDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_employeesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource tbl_employeesBindingSource;
+        private EmloyeesDataSetTableAdapters.tbl_employeesTableAdapter tbl_employeesTableAdapter;
+        public EmloyeesDataSet EmloyeesDataSet;
+        public Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
